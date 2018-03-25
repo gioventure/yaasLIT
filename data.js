f1cae3464b7c43f8b450fcab0a3d4fab437c54f9
nhuq001 <31972361+nhuq001@users.noreply.github.com>
2018-03-24 23:33:09 -0400

    server data
    populated the firebase server

---
+++ data.js
@@ -0,0 +1,107 @@
+happyHourData =
+  [{
+    'name': 'Flanigan',
+    'id': 1,
+    'monday': '5 - 6pm',
+    'tuesday': '5 - 6pm',
+    'wednesday': '5 - 6pm',
+    'thursday': '5 - 6pm',
+    'friday': '4 - 6pm',
+    'saturday': '4 - 6pm',
+    'sunday': '5 - 6pm',
+    'type': 'Food and Drink'
+  },
+  {
+    'name': 'El Palacios de los Jugos',
+    'id': 2,
+    'monday': false,
+    'tuesday': '3 - 6pm',
+    'wednesday': '3 - 6pm',
+    'thursday': '3 - 6pm',
+    'friday': '3 - 6pm',
+    'saturday': false,
+    'sunday': false,
+    'type': 'Food and Drink'
+  },
+  {
+    'name': "Shots",
+    'id': 3,
+    'monday': '3 - 7pm & 10 - 12pm',
+    'tuesday': '3 - 7pm & 10 - 12pm',
+    'wednesday': '3 - 7pm & 10 - 12pm',
+    'thursday': '3 - 7pm & 10 - 12pm',
+    'friday': '3 - 7pm & 10 - 12pm',
+    'saturday': '3 - 7pm & 10 - 12pm',
+    'sunday': '3 - 7pm & 10 - 12pm',
+    'type': 'Just Drinks'
+  },
+  {
+    'name': "Miller Ale House",
+    'id': 4,
+    'monday': '4 - 6pm',
+    'tuesday': '4 - 6pm',
+    'wednesday': '4 - 6pm',
+    'thursday': '4 - 6pm',
+    'friday': '4 - 6pm',
+    'saturday': '4 - 6pm',
+    'sunday': '4 - 6pm',
+    'type': 'Just Drinks'
+  },
+  {
+    'name': "Churchills",
+    'id': 5,
+    'monday': '3 - 6pm & 11 - 2am',
+    'tuesday': '3 - 6pm & 11 - 2am',
+    'wednesday': '3 - 6pm & 11 - 2am',
+    'thursday': '3 - 6pm & 11 - 2am',
+    'friday': '3 - 6pm & 11 - 2am',
+    'saturday': '3 - 6pm & 11 - 2am',
+    'sunday': '3 - 6pm & 11 - 12pm',
+    'type': 'Food and Drink'
+  },
+  {
+    'name': "Eleven",
+    'id': 6,
+    'monday': '5 - 6:30pm',
+    'tuesday': '5 - 6:30pm',
+    'wednesday': '5 - 6:30pm',
+    'thursday': '5 - 6:30pm',
+    'friday': '5 - 6:30pm',
+    'saturday': false,
+    'sunday': false,
+    'type': 'Club'
+  },
+];
+
+locationData = [{
+  'name': 'Flanigan's',
+  'address': '8965 NW 12 St',
+  'website': 'SS'
+},
+{
+  'name': 'El Palacio De Los Jugos',
+  'address': '8965 NW 13 terrace',
+  'website': 'ElPalacioDelosJugos.com'
+},{
+  'name': "Shots",
+  'address': '311 NW 23rd st',
+  'website': 'shotsbar.com'
+},{
+  'name': 'Wet Willies',
+  'address': '760 Ocean Dr.',
+  'website': 'wetwillies.com'
+},{
+  'name': 'Wood Tavern',
+  'address': '2531 NW 2nd Ave',
+  'website': 'Woodtavern.com'
+},{
+  'name': 'Pillsbury Pub',
+  'address': '17890 sw 137 avenue',
+  'website': 'Flanigans.net'
+}
+];
+
+module.exports = {
+  happyData,
+  locationData
+}
